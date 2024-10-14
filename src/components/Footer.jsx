92727594c682638100bc5a34dde8fb0a21406b7b
Akshay <ap7839426@gmail.com>
2024-10-13 21:45:15 -0400

    removed user name from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,11 +9,6 @@ const Footer = () => {
     <footer className="flex justify-center items-center p-4 bg-blue-100 text-gray-700">
       <div className="text-center">
         &copy; {currentYear} Made with love
-        {user?.name && user?.email ? (
-          <span> - {user.name} ({user.email})</span>
-        ) : (
-          <span> - Guest User</span>
-        )}
       </div>
     </footer>
   );
